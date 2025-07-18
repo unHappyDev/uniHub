af97e2299e18bd8dc068992e65bcdab45bf1c696
joaovtrcosta <joaovtr12dev@gmail.com>
2025-07-18 17:16:52 -0300

    update na pagina principal

--- front-end/app/(public)/page.tsx
+++ front-end/app/(public)/page.tsx
@@ -1,21 +1,18 @@
 "use client";
 
+import { About } from "@/components/home/About";
+import { Header } from "@/components/home/Header";
+import { Hero } from "@/components/home/Hero";
 import Navbar from "@/components/layout/Navbar";
+import { ScrollToTopButton } from "@/components/ScrollToTopButton";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <main className="py-8">
-        <section className="text-center py-20">
-          <h1 className="text-4xl font-bold mb-4">
-            Example of Authentication Web App
-          </h1>
-          <p className="text-xl text-muted-foreground max-w-2xl mx-auto">
-            Advance your career with our comprehensive authentication pattern
-          </p>
-        </section>
-      </main>
+      <Header />
+      <Hero />
+      <About />
+      <ScrollToTopButton />
     </div>
   );
 }
--- front-end/components/ScrollToTopButton.tsx
+++ front-end/components/ScrollToTopButton.tsx
@@ -23,7 +23,7 @@ export function ScrollToTopButton() {
     visible && (
       <button
         onClick={scrollToTop}
-        className="fixed bottom-6 right-6 z-50 bg-orange-500 hover:bg-orange-600 text-white p-3 rounded-full shadow-lg transition-all duration-300"
+        className="fixed bottom-6 right-6 z-50 cursor-pointer bg-orange-500 hover:bg-orange-600 text-white p-3 rounded-full shadow-lg transition-all duration-300"
         aria-label="Voltar ao topo"
       >
         <ArrowUp className="w-5 h-5" />
