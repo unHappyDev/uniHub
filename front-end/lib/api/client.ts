cd464beaf4257bef66f6495d2e52e7c30cd13ade
joaovtrcosta <joaovtr12dev@gmail.com>
2025-10-14 09:55:38 -0300

    ajuste

--- front-end/lib/api/client.ts
+++ front-end/lib/api/client.ts
@@ -1,22 +1,11 @@
 import axios, { AxiosInstance } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
+  baseURL: "/api",
   withCredentials: true,
 });
 
-// Interceptor: adiciona o token JWT em todas as requisições
-apiClient.interceptors.request.use(
-  (config) => {
-    if (typeof window !== "undefined") {
-      const token = localStorage.getItem("token");
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-export default apiClient;
+export const apiSpring: AxiosInstance = axios.create({
+  baseURL: "http://localhost:8080",
+  withCredentials: true,
+});
