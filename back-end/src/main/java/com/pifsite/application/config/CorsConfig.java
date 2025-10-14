0c458a61f91f8048ff2060ea8c721a44a71d4955
Wesley <103648122+Wesleyhdias@users.noreply.github.com>
2025-10-14 13:09:26 +0000

    adicionando cursos e um aluno no dataseed do BackEnd

--- back-end/src/main/java/com/pifsite/application/config/CorsConfig.java
+++ back-end/src/main/java/com/pifsite/application/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
